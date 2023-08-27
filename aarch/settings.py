6a8a3223f861d28b41618201a36deb0c48f103b1
Adwait Upadhyaya <adwait627@gmail.com>
2023-08-28 01:52:11 +0545

    set

--- aarch/settings.py
+++ aarch/settings.py
@@ -120,7 +120,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join((BASE_DIR, 'staticfiles_build', 'static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 MEDIA_URLS = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
