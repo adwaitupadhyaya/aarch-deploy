d7d51288800602825a5edaebfe24ec364f52dc99
Adwait Upadhyaya <adwait627@gmail.com>
2023-09-04 10:04:49 +0545

    changes

--- system/urls.py
+++ system/urls.py
@@ -5,6 +5,6 @@ from system.views import LandingPageView, AboutPageView, ContactPageView
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name="landing"),
-    path('/about', AboutPageView.as_view(), name="about"),
-    path('/contact', ContactPageView.as_view(), name="contact"),
+    path('about', AboutPageView.as_view(), name="about"),
+    path('contact', ContactPageView.as_view(), name="contact"),
 ]
